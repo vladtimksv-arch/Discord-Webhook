56ce01684381a3a99fe71a2f3a64597fd66cbd4c
vladtimksv-arch <vladtimksv@gmail.com>
2026-09-17 16:44:20 +0300

    main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ current_time = time.time()
 
 if current_time - news['date'] < 1200:
     print(f"Знайдено нову новину: {news['title']}")
-    prompt = f"Переклади українською цей текст новини Steam. Зроби його читабельним, збережи основний зміст і оформи під повідомлення для Discord. Максимум 1500 символів:\n\n{news['contents'][:4000]}"
+    prompt = f"Переклади українською цей текст новини Steam. Зроби його читабельним, збережи основний зміст і оформи під повідомлення для Discord. Максимум 1900 символів:\n\n{news['contents'][:4000]}"
     
     response = client.models.generate_content(
-        model='gemini-1.5-flash',
+        model='gemini-3.5-flash',
         contents=prompt
     )
     

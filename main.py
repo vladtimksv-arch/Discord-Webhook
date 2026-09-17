feb7456931b7540d87f52455a73bf6bc8a8b294c
vladtimksv-arch <vladtimksv@gmail.com>
2026-09-17 18:03:04 +0300

    main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ prompt = f"""Проаналізуй текст новини (Джерело: {fe
 Текст: {news['contents'][:4000]}"""
 
 response = client.models.generate_content(
-    model='gemini-3.6-flash',
+    model='gemini-3.5-flash',
     contents=prompt
 )
 
